a6f377a30a287eac68f3abe25ed75d0311f0be59
lerbuk <user.name>
2023-01-11 17:00:18 -0600

    gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3d-web-interactive/'
+  base: '/3d-web-interactive/',
+  
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
